1ae4606f71aa713127535c0fdc29c7fa2e042c7c
JoshuaNogues <120759313+JoshuaNogues@users.noreply.github.com>
2023-03-27 17:03:19 -0400

    added some styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,10 +3,12 @@ import { Link } from "react-router-dom";
 import Projects from '../pages/Projects';
 import About from '../pages/About';
 import Contact from '../pages/Contact';
+import logo from '../images/logo.png'
 
 const Navbar = () => {
     return (
       <nav className="navbar">
+        <Link className='navnar-link' to='/'><img className='logo' src={logo} alt='logo'/></Link>
         <Link className="navbar-link" to="/about">About </Link>
         <Link className="navbar-link" to="/projects">Projects </Link>
         <Link className="navbar-link" to="/contact">Contact</Link>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,119 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import me from '../images/me.png'
 
-const home = () => {
+const Typewriter = ({ text, loop }) => {
+  const [currentText, setCurrentText] = useState("");
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentText((prevText) => {
+        const nextChar = text[currentIndex];
+        return nextChar ? `${prevText}${nextChar}` : prevText;
+      });
+      setCurrentIndex((prevIndex) => {
+        if (prevIndex >= text.length) {
+          clearInterval(intervalId);
+        }
+        return prevIndex + 1;
+      });
+    }, 100);
+
+    return () => clearInterval(intervalId);
+  }, [currentIndex, loop, text]);
+
+  return <h1>{currentText}</h1>;
+};
+
+
+const Home = () => {
   return (
-    <div>
-      <h4>Welcome to my Portfolio</h4>
-      <h1 className="hiImJosh">Hi, I'm Josh | Software Engineer</h1>
+    <div className="home">
+      <div className="hero-section">
+        <Typewriter text="Welcome to my Portfolio"/>
+        <h1 className="hiImJosh">Hi, I'm Josh | Software Engineer</h1>
+        <img
+          className="github-logo"
+          src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+        />
+        <img
+          className="linkedin-logo"
+          src="https://www.edigitalagency.com.au/wp-content/uploads/new-linkedin-logo-white-black-png.png"
+        />
+        <img
+          className="email-logo"
+          src="https://cdn-icons-png.flaticon.com/512/6244/6244438.png"
+        />
+        <p>
+          I'm a life-long learner based in Miami, Florida. I love creating
+          beautiful interfaces, web apps, and everything in between!
+        </p>
+      </div>
+      <div className="about">
+        <div>
+          <h2>About Me</h2>
+          <img className="me" src={me} alt="profile-pic"/>
+        </div>  
+        <div>    
+        <h3>I love building stuff</h3>
+        <p>
+          I started playing with computers at a very young age. Since then I
+          have been experimenting with all sorts of technologies that in some way or
+          another led me to work on engineering. Including photography, music, sound
+          engineering, 2d games and more!
+        </p>
+        </div> 
+      </div>
+      <div className="projects">
+        <h2>Projects</h2>
+        <h3>IronSocial</h3>
+        <p>
+          IronSocial is a social media application built with React, Express,
+          and MongoDB, modeled after LinkedIn, designed specifically for
+          IronHackers to collaborate and network. The platform allows
+          IronHackers to post about their current employment, share job
+          opportunities, and offer tips on job hunting, interviews, and related
+          topics. The app aims to foster a deeper sense of community among
+          IronHackers, enabling them to connect and support each other's career
+          goals.
+        </p>
+        <h3>Revate</h3>
+        <p>
+          Revate is a restaurant review website built with Express, MongoDB,
+          JavaScript, and Handlebars. It is similar to Yelp, but exclusively
+          focuses on dining establishments. Here, patrons can share their
+          experiences by leaving reviews on restaurants they have visited, while
+          owners can showcase their restaurants for review.
+        </p>
+        <h3>Alien Invasion</h3>
+        <p>
+          "Alien Invasion" is a space shooter game, reminiscent of the classic
+          arcade game "Galaga", that I independently developed using HTML,
+          Canvas, and Javascript. As a childhood fan of "Galaga", I was inspired
+          to create a game with similar gameplay mechanics and thus brought
+          "Alien Invasion" to life.
+        </p>
+      </div>
+      <div className="contact">
+        <h2>Contact Me</h2>
+        <p>joshuanogues@outlook.com</p>
+      </div>
+      <div>
       <img
-        className="github-logo"
-        src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
-      />
-      <img
-        className="linkedin-logo"
-        src="https://www.edigitalagency.com.au/wp-content/uploads/new-linkedin-logo-white-black-png.png"
-      />
-      <img
-        className="email-logo"
-        src="https://cdn-icons-png.flaticon.com/512/6244/6244438.png"
-      />
-      <p>
-        I'm a life-long leaerner based in Miami, Florida. I love creating
-        beautiful interfaces, web apps, and everything in between!
-      </p>
+          className="github-logo"
+          src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+        />
+        <img
+          className="linkedin-logo"
+          src="https://www.edigitalagency.com.au/wp-content/uploads/new-linkedin-logo-white-black-png.png"
+        />
+        <img
+          className="email-logo"
+          src="https://cdn-icons-png.flaticon.com/512/6244/6244438.png"
+        />
+      </div>
     </div>
   );
 };
 
-export default home;
+export default Home;
